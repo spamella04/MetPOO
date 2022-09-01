73762c5ad6721c6e0a860827e34cac6eb28064f6
Laboratorio C205 <labc205@uam.net.ni>
2022-09-01 09:57:42 -0600

    trabajo 010922

---
+++ Sesion3008/src/sesion3008/Sesion3008.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesion3008;
+
+import modelos.Persona;
+import modelos.Sexo;
+
+/**
+ *
+ * @author labc205
+ */   // dao = data access object
+
+public class Sesion3008 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+ //       Persona maria;
+   //     maria = new Persona(1,"Maria","Lopez","", Sexo.MUJER);
+     //   System.out.println(maria.toString());
+     
+     
+        
+    }
+    
+}
